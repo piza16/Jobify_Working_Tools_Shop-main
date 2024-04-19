5acbaac31666569ffe267aeda4728e81d59a1d95
piza16 <rotempizanti1@gmail.com>
2024-04-19 14:39:10 +0200

    init server package json, connect DB, make some routes,  axios fetch data from server, create all models, nodemon concurrently

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,8 +1,20 @@
-import { Row, Col } from "react-bootstrap"
-import products from "../products"
-import Product from "../Components/Product"
+import { useEffect, useState } from 'react';
+import { Row, Col } from "react-bootstrap";
+import Product from '../Components/Product';
+import axios from 'axios';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+        const { data } = await axios.get('/api/products');
+        setProducts(data);
+    };
+    
+    fetchProducts();
+  }, [])
+  
   return (
     <>
         <h1>מוצרים חדשים</h1>
