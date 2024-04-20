b4fb02de37a0e80b0029129a4f584017ec00558e
piza16 <rotempizanti1@gmail.com>
2024-04-20 14:14:06 +0200

    mongoose, modeling data, organize route & controller for getting product/s, async & error handlers

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -32,6 +32,10 @@ const productSchema = new mongoose.Schema(
       required: true,
       ref: "User",
     },
+    name: {
+      type: String,
+      required: true,
+    },
     image: {
       type: String,
       required: true,
