407a333ac9bc7b98cc652d089cf8ef33c5323bdc
piza16 <rotempizanti1@gmail.com>
2024-05-06 02:43:14 +0300

    last fixes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
-import jwt, { decode } from "jsonwebtoken";
+//import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -22,7 +22,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode).json({
     message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
   });
 };
 
